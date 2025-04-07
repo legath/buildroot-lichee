161eeb4c2217f450a3b00db800923416fdb1a897
Alexander Bulychev <a.f.bulychev@gmail.com>
2025-04-07 19:18:57 +0300

    pymavlink support finished

--- package/pymavlink/pymavlink.mk
+++ package/pymavlink/pymavlink.mk
@@ -9,6 +9,11 @@ PYMAVLINK_SITE = $(call github,ArduPilot,pymavlink,$(PYMAVLINK_VERSION))
 PYMAVLINK_SETUP_TYPE = setuptools
 PYMAVLINK_LICENSE = GPL-2.0
 PYMAVLINK_LICENSE_FILES = COPYING
-PYMAVLINK_DEPENDENCIES= python-future
+PYMAVLINK_DEPENDENCIES= host-pkgconf python3 libxml2 python-future python-lxml host-python-lxml host-python-future
+
+PYMAVLINK_BUILD_OPTS = --skip-dependency-check
+PYMAVLINK_ENV = MDEF=$(@D)/../mavlink-master/message_definitions
+
+
 $(eval $(python-package))
 
